748c24b713532b0ab9a19c1c1234dcce67238abf
Martinez-LMS <leo.ooosla@gmail.com>
2024-03-30 22:01:49 -0300

    alinhado img colocando login e password

--- src/app/components/default-login-layout/default-login-layout.component.ts
+++ src/app/components/default-login-layout/default-login-layout.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { Input } from '@angular/core';
+
 
 @Component({
   selector: 'app-default-login-layout',
@@ -8,5 +10,8 @@ import { Component } from '@angular/core';
   styleUrl: './default-login-layout.component.scss'
 })
 export class DefaultLoginLayoutComponent {
+  @Input() title: string = "";
+  @Input() primaryBtnText: string = "";
+  @Input() secondaryBtnText: string = "";
 
 }
